87ed95c33757a5628afd781aa8b18811d277940b
Yan <yankaikys@gmail.com>
2024-07-07 22:14:00 -0300

    feat(WelcomeScreen): create WelcomeScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { hp, wp } from "@/helpers/common";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "@/constants/theme";
 
 const WelcomeScreen = () => {
   return (
@@ -26,6 +27,27 @@ const WelcomeScreen = () => {
           start={{ x: 0.5, y: 0 }}
           end={{ x: 0.5, y: 0.8 }}
         />
+
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Wallart
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            Every Wallart Tells a Story 🍃🌺
+          </Animated.Text>
+
+          <Animated.View entering={FadeInDown.delay(500).springify()}>
+            <Pressable style={styles.button}>
+              <Text style={styles.buttonText}>Start Explore 🚀</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
       </Animated.View>
     </View>
   );
@@ -44,7 +66,38 @@ const styles = StyleSheet.create({
     width: wp(100),
     height: hp(65),
     bottom: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+  button: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+  buttonText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1,
   },
 });
 
