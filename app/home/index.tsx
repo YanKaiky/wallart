8718794f3071a805f8b2b050bc615c7fd8e719b6
Yan <yankaikys@gmail.com>
2024-07-14 17:01:23 -0300

    feat(HomeScreen): add header

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,14 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/constants/theme";
+import { hp, wp } from "@/helpers/common";
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import { useRef, useState } from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
 
+  const [search, setSearch] = useState<string>("");
+
+  const searchInputRef = useRef(null);
+
   const paddingTop = top > 0 ? top + 10 : 30;
 
   return (
     <View style={[styles.container, { paddingTop }]}>
-      <Text>Home Screen</Text>
+      <View style={styles.header}>
+        <Pressable>
+          <Text style={styles.title}>Wallart</Text>
+        </Pressable>
+        <Pressable>
+          <FontAwesome6
+            name="bars-staggered"
+            size={22}
+            color={theme.colors.neutral(0.7)}
+          />
+        </Pressable>
+      </View>
+
+      <ScrollView contentContainerStyle={{ gap: 15 }}>
+        <View style={styles.searchBar}>
+          <View style={styles.searchIcon}>
+            <Feather
+              name="search"
+              size={24}
+              color={theme.colors.neutral(0.4)}
+            />
+          </View>
+          <TextInput
+            value={search}
+            placeholder="Search for photos..."
+            ref={searchInputRef}
+            style={styles.searchInput}
+            onChangeText={(value: string) => setSearch(value)}
+          />
+          {search && (
+            <Pressable style={styles.closeIcon} onPress={() => setSearch("")}>
+              <Ionicons
+                name="close"
+                size={24}
+                color={theme.colors.neutral(0.6)}
+              />
+            </Pressable>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -17,6 +71,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 15,
+  },
+  header: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: hp(4),
+    fontWeight: theme.fontWeights.semibold,
+    color: theme.colors.neutral(0.9),
+  },
+  searchBar: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: theme.colors.grayBG,
+    backgroundColor: theme.colors.white,
+    padding: 6,
+    paddingLeft: 10,
+    borderRadius: theme.radius.md,
+  },
+  searchIcon: {
+    padding: 8,
+  },
+  searchInput: {
+    flex: 1,
+    borderRadius: theme.radius.sm,
+    paddingVertical: 10,
+    fontSize: hp(1.8),
+  },
+  closeIcon: {
+    backgroundColor: theme.colors.neutral(0.1),
+    padding: 8,
+    borderRadius: theme.radius.sm,
   },
 });
 
